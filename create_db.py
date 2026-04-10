ebdab6005c353de069de2bcc2f92c13d1c739aea
Ritika <ritikaannam16@gmail.com>
2026-04-10 23:31:39 +0530

    Milestone-PPA Final-Submission

--- create_db.py
+++
@@ -1,9 +0,0 @@
-from app import app
-from models import db, Admin
-with app.app_context():
-
-    db.create_all()
-    admin = Admin(username="admin", password="admin123")
-    db.session.add(admin)
-    db.session.commit()
-    print("Database created !! admin added with username: admin and password: admin123")
